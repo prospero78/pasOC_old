114ea207b7da07e0bcc6be62df26cf2b6a7f6a27
prospero78su <prospero.78.su@gmail.com>
2018-03-07 22:17:56 +0200

    10 Добавлены статические члены класса тега.

--- mod/modTag.pas
+++ mod/modTag.pas
@@ -12,9 +12,12 @@ USES
 TYPE
   tTag = CLASS(TObject)
   PRIVATE
+     _id: integer; STATIC;
+     _init: boolean; STATIC;
   PUBLIC
      coord: tCoord;
      CONSTRUCTOR Init(coord_: tCoord; strName: string);
+     CLASS PROCEDURE tTag_Begin; STATIC;
   END;
 
 IMPLEMENTATION
@@ -28,4 +31,15 @@ CONSTRUCTOR tTag.Init(coord_: tCoord; strName: string);
 
   END;
 
+CLASS PROCEDURE tTag.tTag_Begin;
+  BEGIN
+     IF (tTag._init = False) and (tTag._id <> 1) THEN
+        BEGIN
+        tTag._init := True;
+        tTag._id   := 1;
+        END;
+  END;
+
+BEGIN
+  tTag.tTag_Begin;
 END.
